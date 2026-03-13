85cd47f2ae6b2f3bfa4dc3d5638dcd9daa1e7a03
JuSeRDev <juserdev@gmail.com>
2026-03-13 15:16:50 -0500

    cotido de clientes funcionando de forma perfecta

--- src/services/clients/clients.ts
+++ src/services/clients/clients.ts
@@ -6,7 +6,7 @@ import { clients_list } from './components/clients-list';
 import { client_form_edit } from './components/client-edit-form';
 import { handle_client_edit_open } from './components/handlers/handle-client-edit-open';
 import { handle_client_edit } from './components/handlers/handle-client-edit';
-import { deleteClient } from './clients-services';
+import { handle_client_delete } from './components/handlers/handle-client-delete';
 
 
 
@@ -20,30 +20,6 @@ export function content_clients(TOKEN: string, clients: Clients[]): HTMLDivEleme
 
   handle_client_edit_open(list, form_edit)
   handle_client_edit(TOKEN!, form_edit)
-
-  function handle_client_delete(TOKEN: string, container: HTMLDivElement) {
-
-    const icon_delete = container.querySelectorAll<HTMLImageElement>('.icon-delete')
-
-    icon_delete.forEach(icon => {
-
-      icon.addEventListener('click', e => {
-
-        const target = e.target as HTMLImageElement
-        const content = target.closest('.client-file') as HTMLDivElement
-
-        if (!content) return
-
-        const id = content.dataset.id
-
-        deleteClient(TOKEN!, id!)
-
-        window.location.reload()
-      })
-
-    })
-
-  }
 
   handle_client_delete(TOKEN!, list)
 
